32dde8b0bf80bde0c5f768779ba7dbb12d5f7364
Nick Kirsch <nick@atsu.io>
2020-03-06 09:57:58 -0800

    Fix issues #1 & #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,29 @@
 #!/bin/sh -l
 
-if [ -d /github/workspace ]; then
-	cd /github/workspace
+if [ -z "${GITHUB_WORKSPACE}" ]; then
+	GITHUB_WORKSPACES=/github/workspace
+fi
+
+if [ -d "${GITHUB_WORKSPACE}" ] ;then
+	cd ${GITHUB_WORKSPACE}
 fi
 
 if [ -f "$1" ]; then
-	echo "running kubeyaml on $1"
+	echo "running /kubeyaml on $1"
 	/kubeyaml < "$1"
 fi
 
 if [ -d "$1" ]; then
-	echo "running kubeyaml on DIR yaml $1"
-	find $1 -iname \*.yaml -type f -exec sh -c "cat {} | /kubeyaml" \;
-	find $1 -iname \*.yml -type f -exec sh -c "cat {} | /kubeyaml" \;
+	echo "running find yaml on DIR $1"
+	find $1 -iname \*.yaml -type f -print0 | xargs -0 -t -n1 sh -c 'cat $@ | /kubeyaml' \;
+	if [ $? -ne 0 ]; then
+		echo "error, exiting"
+		exit 1
+	fi
+	echo "running find yml on DIR $1"
+	find $1 -iname \*.yaml -type f -print0 | xargs -0 -t -n1 sh -c 'cat $@ | /kubeyaml' \;
+	if [ $? -ne 0 ]; then
+		echo "error, exiting"
+		exit 1
+	fi
 fi
