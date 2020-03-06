a8ead6e19614add46936863d3995644493a7a855
Nick Kirsch <nick@atsu.io>
2020-03-05 17:26:58 -0800

    file or director

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/sh -l
 
-echo "running kubeyaml on $1"
-/kubeyaml < /github/workspace/"$1"
+if [ -d /github/workspace ]; then
+	cd /github/workspace
+fi
+
+if [ -f "$1" ]; then
+	echo "running kubeyaml on $1"
+	/kubeyaml < "$1"
+fi
+
+if [ -d "$1" ]; then
+	echo "running kubeyaml on DIR yaml $1"
+	find $1 -iname \*.yaml -type f -exec sh -c "cat {} | /kubeyaml" \;
+	find $1 -iname \*.yml -type f -exec sh -c "cat {} | /kubeyaml" \;
+fi
