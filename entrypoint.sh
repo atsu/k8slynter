611ab44581a498b578e41718572a01475ea2a203
Nick Kirsch <nick@atsu.io>
2020-03-06 15:29:22 -0800

    echo cmds

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,14 +19,14 @@ fi
 
 if [ -d "$1" ]; then
 	echo "running find yaml on DIR $1"
-	find $1 -type f -iname \*.yaml -print0 | xargs -r -0 -n1 sh -c 'cat $@ | /kubeyaml' \;
+	find $1 -type f -iname \*.yaml -print0 | xargs -r -0 -t -n1 sh -c 'cat $@ | /kubeyaml' \;
 	if [ $? -ne 0 ]; then
 		echo "error, exiting"
 		exit 1
 	fi
 
 	echo "running find yml on DIR $1"
-	find $1 -type f -iname \*.yml -print0 | xargs -r -0 -n1 sh -c 'cat $@ | /kubeyaml' \;
+	find $1 -type f -iname \*.yml -print0 | xargs -r -0 -t -n1 sh -c 'cat $@ | /kubeyaml' \;
 	if [ $? -ne 0 ]; then
 		echo "error, exiting"
 		exit 1
